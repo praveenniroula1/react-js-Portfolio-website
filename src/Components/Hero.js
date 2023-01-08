7d70c1f77d1507621c16b74d79720b25464c7f0b
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2023-01-08 14:51:22 +1100

    nav bar made

---
+++ src/Components/Hero.js
@@ -0,0 +1,22 @@
+import React from "react";
+import photo from "../img/banner.png";
+
+export const Hero = () => {
+  return (
+    <div className="intro container ">
+      {/* left */}
+      <div className="i-left">
+        <div className="i-name">
+          <span>Hy! I Am</span>
+          <span>Praveen Niroula</span>
+          <span>
+            Full-Stack-Developer with high level of experience in web designing
+            and development.
+          </span>
+        </div>
+      </div>
+
+      {/* right */}
+    </div>
+  );
+};
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,6 +1,5 @@
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -9,7 +8,7 @@ import logo from "../img/logoo.png";
 
 const NavBar = () => {
   return (
-    <div className="navBar container-fluid">
+    <div className="navBar bg-dark container p-1">
       {[false].map((expand) => (
         <Navbar key={expand} expand={expand} className="mb-3">
           <Container fluid>
@@ -20,46 +19,43 @@ const NavBar = () => {
               </a>
             </Navbar.Brand>
             <Navbar.Toggle
-              className="toggle bg-light fs-3 p-3 "
+              className="toggle bg-light text-dark fs-3 p-3 fw-bold"
               aria-controls={`offcanvasNavbar-expand-${expand}`}
-            ></Navbar.Toggle>
+            >
+              Menu <i class="fa-solid fa-bars"></i>
+            </Navbar.Toggle>
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
               placement="end"
+              className="bg-dark text-light fs-3"
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Menu
+                  <a className="navbar-brand " href="#">
+                    <img src={logo} width="300px" height="250px" alt="" />
+                  </a>
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                  <NavDropdown
-                    title="Dropdown"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
+                  <Nav.Link href="#action1"></Nav.Link>
+                  <Nav.Link href="#action1">
+                    <i class="fa-solid fa-house-user"></i> Home
+                  </Nav.Link>
+                  <Nav.Link href="#action2">
+                    <i class="fa-brands fa-studiovinari"></i> Skills
+                  </Nav.Link>
+                  <Nav.Link href="#action2">
+                    <i class="fa-solid fa-diagram-project"></i> Projects
+                  </Nav.Link>
+                  <Nav.Link href="#action2">
+                    <i class="fa-solid fa-address-card"></i> About Me
+                  </Nav.Link>
+                  <Nav.Link href="#action2">
+                    <i class="fa-solid fa-address-book"></i> Contact Me
+                  </Nav.Link>
                 </Nav>
-                {/* <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form> */}
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
