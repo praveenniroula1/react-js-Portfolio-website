7be048a54f317ef72acb8df4b5d506ae67177bcb
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2023-01-11 11:08:29 +1100

    changing the layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import Hero from "./Components/Hero";
-import NavBar from "./Components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <Hero />
     </div>
   );
