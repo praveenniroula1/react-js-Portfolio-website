ff2eddac15cbf258db0427218b3a018e2accc471
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2023-02-04 11:57:16 +1100

    updated

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -12,29 +12,33 @@ const Skills = () => {
       </h1>
       <div className="logo container ">
         <img className="imgg " src={svg} alt="logo"></img>
-        <Card className="card">
+
+        <Card className="card2 container ">
           <Card.Body>
             {/* <Card.Title>HTML5</Card.Title> */}
-            <Card.Text className="fs-3 ">
-              <i class="htmll fa-brands fa-html5 "> HTML5</i>
-              <i class="react fa-brands fa-react "> React JS</i>
-              <i class="css fa-brands fa-css3-alt "> CSS3</i>
-              <i class="js fa-brands fa-js"> javaScript</i>
-              <i class="bootstrap fa-brands fa-bootstrap"> BootStrap</i>
-              <i class="npm fa-brands fa-npm"> N P M</i>
-              <i class="node fa-brands fa-node"> Node-JS</i>
-              <i class="figma fa-brands fa-figma"> Figma</i>
-              <i class="vscode fa-solid fa-code"> Vs Code</i>
-              <i class="git fa-brands fa-github"> GitHub</i>
+            <Card.Text className="fs-4 container">
+              <i class="qqq item1 htmll fa-brands fa-html5 "> HTML5</i>
+              <i class="qqq item2 react fa-brands fa-react "> React JS</i>
+              <i class="qqq item3 css fa-brands fa-css3-alt "> CSS3</i>
+              <i class="qqq item4 js fa-brands fa-js"> javaScript</i>
+              <i class="qqq item5 bootstrap fa-brands fa-bootstrap">
+                {" "}
+                BootStrap
+              </i>
+              <i class="qqq item6 npm fa-brands fa-npm"> N P M</i>
+              <i class="qqq item7  node fa-brands fa-node"> Node-JS</i>
+              <i class="qqq item8 figma fa-brands fa-figma"> Figma</i>
+              <i class="qqq item9 vscode fa-solid fa-code"> Vs Code</i>
+              <i class="qqq item10 git fa-brands fa-github"> GitHub</i>
             </Card.Text>
           </Card.Body>
         </Card>
         {/* <ul className="row fs-3">
           <li className="col-sm-6 col-lg-3">
-            <i class="htmll fa-brands fa-html5 "> HTML5</i>
+            <i class="qqq htmll fa-brands fa-html5 "> HTML5</i>
           </li>
           <li className="col-sm-6 col-lg-3">
-            <i class="css fa-brands fa-css3-alt "> CSS3</i>
+            <i class="qqq css fa-brands fa-css3-alt "> CSS3</i>
           </li>
           <li className="col-sm-6 col-md-6 col-lg-3">
          
